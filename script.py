317d3404f7f3a375d83183f95a0982956a1e1ddc
Raksha Hegde <rakshahegde@Rakshas-MBP.lan>
2023-05-05 00:46:02 -0400

    date mod in archive folder

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import boto3
 import os
 import tarfile
 import glob
+import datetime
 
 # create an S3 client object
 s3 = boto3.client('s3')
@@ -24,19 +25,25 @@ os.makedirs(current_path, exist_ok=True)
 destination_path = os.path.join(data_set_path, 'providers.tar.gz')
 
 # download the file from S3 to the local destination path
-s3.download_file(bucket_name, file_path, destination_path)
+try:
+  s3.download_file(bucket_name, file_path, destination_path)
+except:
+  print("An exception occurred while downloading file from Ideon S3 Bucket")
+
 
 #Extracting the file
-with tarfile.open(f'{data_set_path}/providers.tar.gz', 'r:gz') as tar:
+try:
+  with tarfile.open(f'{data_set_path}/providers.tar.gz', 'r:gz') as tar:
     tar.extractall(f'{data_set_path}/')
-
+except:
+  print("An exception occurred while extracting from tar file")
 
 #Moving the file
 for file_path in glob.glob(os.path.join(data_set_path, '*.jsonl')):
     file_name = os.path.basename(file_path)
     new_file_path = os.path.join(current_path, file_name)
     os.rename(file_path, new_file_path)
- 
+
 source_bucket_name = 'nyu-capstone-2023'
 destination_bucket_name = 'nyu-capstone-2023'
 
@@ -46,6 +53,8 @@ destination_folder_path = 'data/archive/'
 
 # check if the source folder is not empty
 response = s3.list_objects_v2(Bucket=source_bucket_name, Prefix=source_folder_path)
+now = datetime.datetime.now()
+date_string = now.strftime("%Y%m%d")
 if 'Contents' in response:
     # list all JSONL files in the source folder
     response = s3.list_objects_v2(Bucket=source_bucket_name, Prefix=source_folder_path, Delimiter='/')
@@ -53,15 +62,21 @@ if 'Contents' in response:
         if obj['Key'].endswith('.jsonl'):
             # construct the source and destination file paths
             source_key = obj['Key']
-            destination_key = destination_folder_path + source_key.split('/')[-1]
+            source_file_name = source_key.split('/')[-1]
+            destination_file_name = date_string + '.jsonl'
+            destination_key = destination_folder_path + destination_file_name
 
             # copy the file to the destination folder
-            s3.copy_object(Bucket=destination_bucket_name, CopySource={'Bucket': source_bucket_name, 'Key': source_key}, Key=destination_key)
+            try:
+                s3.copy_object(Bucket=destination_bucket_name, CopySource={'Bucket': source_bucket_name, 'Key': source_key}, Key=destination_key)
+            except:
+                print("An exception occurred while copying file to archive in s3")
 
             # delete the file from the source folder
-            s3.delete_object(Bucket=source_bucket_name, Key=source_key)
-        else:
-            print("The current folder is empty.")
+            try:
+                s3.delete_object(Bucket=source_bucket_name, Key=source_key)
+            except:
+                print("An exception occurred while deleting file in current folder in s3")
 
 
 # Set the name of the S3 bucket
@@ -70,18 +85,22 @@ bucket_name = 'nyu-capstone-2023'
 # Set the name of the S3 folder to upload the files to
 s3_folder = 'data/current'
 
+print("Script is running....")
+
 for filename in os.listdir(current_path):
     if filename.endswith('.jsonl'):
         # Set the local file path
         local_file = os.path.join(current_path, filename)
-        
         # Set the S3 object key
         s3_key = f'{s3_folder}/{filename}'
-        
         # Upload the file to S3
         with open(local_file, 'rb') as f:
-            s3.upload_fileobj(f, bucket_name, s3_key)
+            try:
+                s3.upload_fileobj(f, bucket_name, s3_key)
+            except:
+                print("An exception occurred while deleting file in current folder in s3")
 
+print("Script End")
 
 #Deleting the file from local directory
 files = glob.glob(f'{data_set_path}/*.tar.gz')
